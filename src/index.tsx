15d4c52c1ef63d2d1efba39d02b8d45fa03b7d71
Krzysztof Wasilak <wasilakkrzysztof@protonmail.com>
2023-02-09 19:09:26 +0100

    feat: setup basic app structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {App} from './App';
+import './index.css';
+
+const $root = document.getElementById('root');
+
+if ($root) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    $root
+  );
+}
