09e125a71a14f28379046246b1e8453c384f7238
Krzysztof Wasilak <wasilakkrzysztof@protonmail.com>
2023-02-09 20:42:09 +0100

    feat: remove explicit onClick prop in ButtonWithSpinner

--- src/components/ButtonWithSpinner/ButtonWithSpinner.tsx
+++ src/components/ButtonWithSpinner/ButtonWithSpinner.tsx
@@ -9,9 +9,9 @@ type PropsType = React.ButtonHTMLAttributes<HTMLButtonElement> &
     showSpinner?: boolean;
   }>;
 
-export function ButtonWithSpinner({children, showSpinner, onClick, ...buttonProps}: PropsType) {
+export function ButtonWithSpinner({children, showSpinner, ...buttonProps}: PropsType) {
   return (
-    <button type="button" className={styles.button} onClick={onClick} {...buttonProps}>
+    <button type="button" className={styles.button} {...buttonProps}>
       {showSpinner ? <Spinner size="s" color="white" dataTestId="button_spinner" /> : children}
     </button>
   );
