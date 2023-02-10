c488f0f8f88057779bec4721f9d8e72aac522405
Krzysztof Wasilak <wasilakkrzysztof@protonmail.com>
2023-02-10 12:42:46 +0100

    feat: add spinner to ContactList

--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import {Spinner} from '../Spinner';
 import ContactListItem, {ContactDataType} from './ContactListItem';
 import styles from './ContactList.module.css';
 
@@ -9,28 +10,46 @@ type PropsType = Readonly<{
     emptyList: string;
   };
   contacts: ReadonlyArray<ContactDataType>;
+  showSpinner?: boolean;
   onContactClick: (contactId: string) => unknown;
 }>;
 
-export function ContactList({copy, contacts, onContactClick}: PropsType) {
+export function ContactList({copy, contacts, showSpinner, onContactClick}: PropsType) {
   const isEmptyList = contacts.length === 0;
 
-  const contactsNodes = React.useMemo(
-    () => contacts.map((contact) => <ContactListItem key={contact.id} onClick={onContactClick} {...contact} />),
-    [contacts, onContactClick]
-  );
+  const contactListContent = React.useMemo(() => {
+    if (showSpinner) {
+      return (
+        <div className={styles.contentBox}>
+          <Spinner dataTestId="contact_list_spinner" />
+        </div>
+      );
+    }
+
+    if (isEmptyList) {
+      return (
+        <div className={styles.contentBox}>
+          <p className={styles.emptyInfo} data-testid="contact_list_empty_info">
+            {copy.emptyList}
+          </p>
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.list}>
+        {contacts.map((contact) => (
+          <ContactListItem key={contact.id} onClick={onContactClick} {...contact} />
+        ))}
+      </div>
+    );
+  }, [showSpinner, isEmptyList, copy.emptyList, contacts, onContactClick]);
 
   return (
     <div data-testid="contact_list">
       <h2 className={styles.title}>{copy.title}</h2>
 
-      {isEmptyList ? (
-        <div className={styles.emptyListInfo} data-testid="contact_list_empty_info">
-          <p>{copy.emptyList}</p>
-        </div>
-      ) : (
-        <div className={styles.list}>{contactsNodes}</div>
-      )}
+      {contactListContent}
     </div>
   );
 }
